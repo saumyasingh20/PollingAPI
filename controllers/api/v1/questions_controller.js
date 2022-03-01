9ed50db195f52ff31bc8e674e52aec53bc62b30e
Saumya Singh <53351628+saumyasingh20@users.noreply.github.com>
2022-03-01 16:24:46 +0530

    created new question

--- controllers/api/v1/questions_controller.js
+++ controllers/api/v1/questions_controller.js
@@ -4,8 +4,23 @@ const Option = require('../../../models/option');
 
 module.exports.createNewQuestion = async function(req,res){
     try{
-
+        let question = await Question.create(req.body);
+        if(question){
+            //return 200 success response if it is created
+            return res.status(200).json({
+                question,
+                data : { message : "New Question created Sucessfully !" },
+            });
+        }else{
+            // else return status 500 internal server error
+            return res.status(500).json({
+                data : { message : "Internal Server Error" }
+            });
+        }  
     }catch(err){
-
+        console.log("Error in creating question",err);
+        return res.status(500).json({
+            data : { message : "Internal Server Error" }
+        });
     }
 }
